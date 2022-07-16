5112b1eb4567a86b281eb33d3babc2f8adbf2d17
JackCokebb <reo2726@gmail.com>
2022-07-16 14:24:32 +0900

    22.07.16 - App.js init

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,13 +1,13 @@
 
 import { View } from 'react-native';
 
-export default function App() {
+//Remember! component is the way we can configure the OS.
+export default function App() { //2.4 01:58!!!!!!!!!!!!!!!!
   return (
-    <View>
-        <View></View>
-        <View></View>
-        <View></View>
-      
+    <View style={{flexDirection: "row"}}>
+        <View style={{width:100, height:100, backgroundColor: "tomato"}}></View>
+        <View style={{width:100, height:100, backgroundColor: "teal"}}></View>
+        <View style={{width:100, height:100, backgroundColor: "orange"}}></View>
     </View>
   );
 }
