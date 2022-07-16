71a260aaa0a6847f4413a91c4c9b6039ea036a1e
JackCokebb <reo2726@gmail.com>
2022-07-16 15:20:31 +0900

    22.07.16 - flex container

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,14 +1,21 @@
 
 import { View } from 'react-native';
 
-//Remember! component is the way we can configure the OS.
-export default function App() { //2.4 01:58!!!!!!!!!!!!!!!!
+// Remember! component is the way we can configure the OS.
+// container View is already flex container by default.
+// and by default all the Flex direction is Column in contrast to web.
+
+// normally width and height will not be fixed, because it's not reactive.
+// flex: 1 is proportion which means if there is 3 views witgh same flex: 1,
+// they are gonna distribute their size equally 1:1:1
+export default function App() { 
   return (
-    <View style={{flexDirection: "row"}}>
-        <View style={{width:100, height:100, backgroundColor: "tomato"}}></View>
-        <View style={{width:100, height:100, backgroundColor: "teal"}}></View>
-        <View style={{width:100, height:100, backgroundColor: "orange"}}></View>
+    <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: "tomato"}}></View>
+        <View style={{flex: 1, backgroundColor: "teal"}}></View>
+        <View style={{flex: 1, backgroundColor: "orange"}}></View>
     </View>
   );
 }
 
+// 07:33!!!!!
