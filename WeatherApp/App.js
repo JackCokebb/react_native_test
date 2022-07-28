fcd86eacf48a0be8e3d2512ff23e082e04ca6499
JackCokebb <reo2726@gmail.com>
2022-07-28 22:44:30 +0900

    22.07.28 - weather app Scroll view

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+// With Dimensions we can get the size of the screen
+import { View, Text, StyleSheet, ScrollView,Dimensions } from 'react-native';
 
 // everything in react native is component.
 // for example, there is no auto scroll down in React in contrast to web.
 // So we have to use ScrollView component
 
+// get width from inside of the object, change the name of width to SCREEN_WIDTH
+const {height, width:SCREEN_WIDTH} = Dimensions.get("window");
+//console.log(height, width);
+// width:SCREEN_WIDTH -means-> width == SCREEN_WIDTH
+
+
+// Scroll view's prop -> 
+//  pagingEnabled: we can not freely scroll, instead it allows us to make pages
+//  showHorizontalScrollIndicator: if you set this as false -> hide the horizontal scroll bar 
+//  indicatorStyle: style of scroll bar only on ios(of cause we have to set the showHorizontalScrollIndicator as true)
 export default function App() { 
   return (
     <View style={styles.container}>
@@ -13,7 +24,13 @@ export default function App() {
         <View style={styles.city}>
             <Text style={styles.cityName}>Seoul</Text>
         </View>
-        <ScrollView style={styles.weather}>
+        <ScrollView 
+        horizontal 
+        pagingEnabled 
+        showsHorizontalScrollIndicator={false}
+        indicatorStyle="white"
+        contentContainerStyle={styles.weather}
+        >
             <View style={styles.day}>
                 <Text style={styles.temp}>35</Text>
                 <Text style={styles.desc}>Sunny</Text>
@@ -52,11 +69,10 @@ const styles = StyleSheet.create({
         fontWeight: "600",
     },
     weather:{
-        flex: 3,
         
     },
     day:{
-        flex: 1,
+        width: SCREEN_WIDTH,
         alignItems: "center",
     },
     temp:{
